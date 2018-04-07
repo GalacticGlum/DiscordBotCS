cc04908d72988423dbdf9f1e969ca36aa5f67291
GalacticGlum <nerftactic09@gmail.com>
2018-04-06 23:13:02 -0400

    Added more testing code.
    Can chat from Python application through standard input. Currently, the standard input is not async. Thus, it blocks the thread. Since this is only a testing feature, there is no reason to bother with fixing it. However, if we want to read in standard input down the read, we should do it async (asyncio).

--- csbot/application.py
+++ csbot/application.py
@@ -8,6 +8,8 @@ client = DiscordClient()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    channel = client.get_channel(367469096557871127) 
+    await channel.send('Your favourite teacher—and bot—me—has arrived!')
 
 @client.event
 async def on_message(message):
@@ -15,4 +17,13 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+async def console_read_task():
+    await client.wait_until_ready()
+
+    channel = client.get_channel(367469096557871127)
+    while not client.is_closed():
+        message = input()
+        await channel.send(message)
+
+client.loop.create_task(console_read_task())
 client.run(config['client-token'])

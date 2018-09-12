a1ddfb714543e9409710f358d4d7c88e498e8d41
Shon Verch <glum@static.6.107.243.136.clients.your-server.de>
2018-09-13 01:42:05 +0200

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-python3 csbot/application.py > $(date -d "today" +"%Y%m%d%H%M").log &
+pkill -f discordbot
+source venv/bin/activate
+
+cpath="$(realpath .)"
+command="python3 $cpath/csbot/application.py &> $cpath/$(date -d "today" +"%Y%m%d%H%M".log) &"
+bash -c "exec -a discordbot $command"
